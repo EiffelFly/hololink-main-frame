b858976b60694d4862140bb89d708ad2b938eb03
eiffelfly <eric525282@gmail.com>
2020-07-07 13:50:59 +0800

    Model: 增加Project-"created_at/by"

--- hololink/project/models.py
+++ hololink/project/models.py
@@ -4,10 +4,24 @@ from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+def now():
+    return timezone.localtime(timezone.now())
+
 class Project(models.Model):
 
     name = models.CharField(
         verbose_name=_('Name'),
         max_length=256,
         blank=True,
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'),
+        auto_now_add=True
+    )
+
+    created_by = models.ForeignKey(
+        verbose_name=_('Created by'),
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
     )
