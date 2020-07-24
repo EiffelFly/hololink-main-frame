20e571a5c0bed28df21334e299ce813f7f562ebf
eiffelfly <eric525282@gmail.com>
2020-07-24 09:09:21 +0800

    Major:Templates - add default font-family and designed the add project page

--- hololink/project/forms.py
+++ hololink/project/forms.py
@@ -1,8 +1,36 @@
 from django import forms
 from .models import Project
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 class ProjectForm(forms.ModelForm):
 
+    name = forms.CharField(
+        label='Project name',
+        required = True,
+        widget=forms.TextInput(
+            attrs={
+                'style':''
+            },
+        ),
+    )
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if Project.objects.filter(name=name).exists():
+            self.add_error(
+                'name',
+                ValidationError(
+                    _('The project name has already been used.'),
+                    code='invalid'
+                )
+            )
+        return name
+
+
+
+
+
     class Meta:
         model = Project
-        exclude = ['created_by', 'created_at', 'project_basestone_keyword_sum', 'project_stellar_keyword_sum']
+        exclude = ['created_by', 'created_at', 'project_basestone_keyword_sum', 'project_stellar_keyword_sum', 'slug']
