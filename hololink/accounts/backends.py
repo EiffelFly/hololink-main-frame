36e307ade229d245d7ef9b9a3ff67038e689f951
eiffelfly <eric525282@gmail.com>
2020-08-11 09:07:08 +0800

    modified backends error exception import issue

--- hololink/accounts/backends.py
+++ hololink/accounts/backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.core.exceptions import MultipleObjectsReturned
 
 UserModel = get_user_model()
 
