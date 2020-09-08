3ac06d5ff23ff780fc6b03af8b56a8224ff7dbb9
eiffelfly <eric525282@gmail.com>
2020-09-08 15:23:56 +0800

    [Project admin]-modified admin list_display

--- hololink/project/admin.py
+++ hololink/project/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['name', 'last_edited_time']
-    filter_horizontal = ('articles',)
+    list_display = ['name', 'created_by' ,'last_edited_time']
+    filter_horizontal = ('articles_project_owned',)
 
 admin.site.register(Project, ProjectAdmin)
