5278bc115a9a0e2712c45e3385102fe4877d693a
eiffelfly <eric525282@gmail.com>
2020-10-20 11:47:29 +0800

    [D3]-try to rebuild the force diagram

--- hololink/project/views.py
+++ hololink/project/views.py
@@ -137,7 +137,7 @@ def project_articles(request, projectnameslug, **kwargs):
         'count_article_stellar':count_article_stellar,
     }  
 
-    return render(request, 'project_dashboard_articles.html', context)
+    return render(request, 'project_dashboard_articles_2.html', context)
 
 def project_dashboard(request, projectnameslug, **kwargs):
     if not request.user.is_authenticated:
