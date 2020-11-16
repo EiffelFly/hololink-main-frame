026ccde4292612b6a7050175c9f76bf6e8fd2884
eiffelfly <eric525282@gmail.com>
2020-11-16 16:47:08 +0800

    [Telescope]-add ajax to post new telescope configuration and relative view/url

--- hololink/project/urls.py
+++ hololink/project/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import projects_list, project_detail, create_newproject, project_dashboard, project_articles, project_hologram, galaxy_setting, deliver_D3, galaxy_telescope
+from .views import projects_list, project_detail, create_newproject, project_dashboard, project_articles, project_hologram, galaxy_setting, deliver_D3, galaxy_telescope, add_telescope_configuration
 
 app_name = 'project'
 
 urlpatterns = [
      path('', projects_list, name='projects_list'),
      path('create/', create_newproject, name='create_newproject'),
+     path('ajax/add_telescope_configuration/', add_telescope_configuration, name='add_telescope_configuration'),
      path('<slug:projectnameslug>/', project_dashboard, name='project_dashboard'),
      path('<slug:projectnameslug>/articles', project_articles, name='project_articles'),
      path('<slug:projectnameslug>/hologram', project_hologram, name='project_hologram'),
